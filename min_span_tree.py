eb55905138f3380ca483231b5dd23e546003d511
Pavel Bolotin <Pavel.Bolotin@gmail.com>
2020-11-27 13:54:57 +0700

    Добавить функцию load_matrix_from_file которая считывает данные из файла (матрицу смежности), заносит их в двумерный список, и выводит

--- min_span_tree.py
+++ min_span_tree.py
@@ -28,6 +28,9 @@ D;8;0;5;0;7
 E;4;0;1;7;0
 
 В левом верхнем углу указывается количество вершин в графе (в примере 5).
+
+Разделитель данных - ; - точка с запятой
+
 Далее, по первой горизонтали и вертикали, указываются имена вершин.
 Остальные места заполнены матрицей смежности для взвешенного
 неориентированного графа.
@@ -39,7 +42,17 @@ E;4;0;1;7;0
 def check_number_of_arguments():
     if 2 != len(sys.argv): return False
 
+def load_matrix_from_file():
+    data_file = open(sys.argv[1])
+    data = [] 
+    for i in data_file:
+        data.append(i.strip().split(";"))
+        
+    data_file.close()
+    print(data)
+
 if __name__ == "__main__":
     if(check_number_of_arguments() == False):
         print_help()
         exit()
+    load_matrix_from_file()
